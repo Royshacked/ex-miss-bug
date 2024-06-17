3d9c94b3ee644cf08fd1dc0ee8308d600e7e26fb
Royshacked <roy.shacked@mail.huji.ac.il>
2024-06-17 19:03:08 +0300

    Basic styling of Preview and List

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -1,8 +1,8 @@
 
 
-export function BugPreview({bug}) {
+export function BugPreview({ bug }) {
 
-    return <artice>
+    return <artice class="bug-preview">
         <h4>{bug.title}</h4>
         <h1>🐛</h1>
         <p>Severity: <span>{bug.severity}</span></p>
