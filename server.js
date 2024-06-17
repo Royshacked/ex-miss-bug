5387ecfacaa77f0b8d68c3100c96222ab9093cae
Royshacked <roy.shacked@mail.huji.ac.il>
2024-06-17 16:53:07 +0300

    Create api/bug

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import express from 'express'
 
+var bugs = [
+    {
+        _id: "abc123",
+        title: "Cannot save a new car",
+        description: "problem when clicking Save",
+        severity: 3,
+        createdAt: 1542107359454,
+    }
+]
+
 const app = express()
 app.get('/', ((req, res) => res.send('hello there')))
 app.listen(3030, () => console.log('Server is ready'))
+
+app.get('/api/bug', ((req, res) => {
+    res.send(bugs)
+}))
+
+
