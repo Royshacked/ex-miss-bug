53a060bb46732e4d0f227a02ed3ded7770341597
Royshacked <roy.shacked@mail.huji.ac.il>
2024-06-25 10:02:16 +0300

    Back to Hash

--- server.js
+++ server.js
@@ -102,9 +102,9 @@ app.post('/api/bug', ((req, res) => {
         })
 }))
 
-app.get('/**', (req, res) => {
-    res.sendFile(path.resolve('public/index.html'))
-})
+// app.get('/**', (req, res) => {
+//     res.sendFile(path.resolve('public/index.html'))
+// })
 
 
 
