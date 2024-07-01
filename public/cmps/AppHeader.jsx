f586467020a396d6f40f48ba3c65e3f28d6b7482
Royshacked <roy.shacked@mail.huji.ac.il>
2024-07-01 14:35:49 +0300

    Implement login and signup functionality

--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,12 +1,29 @@
 const { NavLink } = ReactRouterDOM
 const { useState } = React
 
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
 import { userService } from '../services/user.service.js'
 import { LoginSignup } from './LoginSignup.jsx'
 import { UserMsg } from './UserMsg.jsx'
 
 export function AppHeader() {
   const [user, setUser] = useState(userService.getLoggedInUser())
+
+  function logOut() {
+    userService.logout()
+      .then(() => {
+        setUser(null)
+        showSuccessMsg('Logged out successfully')
+      })
+      .catch((err) => {
+        showErrorMsg('Could\'nt log out')
+        console.log(err)
+      })
+  }
+
+  function onSetUser(user) {
+    setUser({ ...user })
+  }
 
   return (
     <header>
@@ -16,11 +33,7 @@ export function AppHeader() {
         <NavLink to="/about">About</NavLink>
       </nav>
       <h1>Bugs are Forever</h1>
-      {user ?
-        <section>User:{user.fullname}</section> :
-
-
-        <LoginSignup />}
+      {user ? <section>Hello {user.fullname} <button onClick={logOut}>LogOut</button></section> : <LoginSignup onSetUser={onSetUser} />}
     </header>
   )
 }
--- public/cmps/LoginSignup.jsx
+++ public/cmps/LoginSignup.jsx
@@ -1,21 +1,63 @@
 const { useState } = React
 
-export function LoginSignup() {
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { userService } from "../services/user.service.js"
+
+export function LoginSignup({ onSetUser }) {
     const [isSignedUp, setIsSignedUp] = useState(false)
+    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+
+    function handleChange({ target }) {
+        const { name, value } = target
+
+        setCredentials(prevCred => ({ ...prevCred, [name]: value }))
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault()
+
+        isSignedUp ? login(credentials) : signup(credentials)
+    }
+
+    function login(credentials) {
+        userService.login(credentials)
+            .then((user) => {
+                showSuccessMsg('Logged in successfully')
+                onSetUser(user)
+            })
+            .catch((err) => {
+                showErrorMsg('Could\'nt log in')
+                console.log(err)
+            })
+    }
+
+    function signup(credentials) {
+        userService.signup(credentials)
+            .then((user) => {
+                showSuccessMsg('Signed up successfully')
+                onSetUser(user)
+            })
+            .catch((err) => {
+                showErrorMsg('Could\'nt sign up')
+                console.log(err)
+            })
+    }
+
+
     return <section className="login-signup">
         <button onClick={() => setIsSignedUp(!isSignedUp)}>{!isSignedUp ? 'Already a member? SignIn' : 'Not a member? SignUp'}</button>
 
-        <form >
+        <form onSubmit={handleSubmit}>
             <label htmlFor="username">
-                <input id="username" type="text" placeholder="Username?" />
+                <input id="username" type="text" placeholder="Username?" name="username" onChange={handleChange} value={credentials.username} required />
             </label>
 
             <label htmlFor="password">
-                <input id="password" type="text" placeholder="Password?" />
+                <input id="password" type="password" placeholder="Password?" name="password" onChange={handleChange} value={credentials.password} required />
             </label>
 
             {!isSignedUp && <label htmlFor="fullname">
-                <input id="fullname" type="text" placeholder="Fullname?" />
+                <input id="fullname" type="text" placeholder="Fullname?" name="fullname" onChange={handleChange} value={credentials.fullname} required />
             </label>}
 
             <button>{!isSignedUp ? 'SignUp' : 'SignIn'}</button>
